4748103eb3afd27d06fc14276f35ae6b57d34fc5
CW - Roberto Olvera Hernandez <rolvhdez@ccg.unam.mx>
2026-03-02 19:14:08 +0000

    [changed] Script is now used to launch a Swiss Army Knife job on DNAnexus

--- 04_other-utilities/reformat_gsav2-chip.sh
+++ 04_other-utilities/reformat_gsav2-chip.sh
@@ -10,34 +10,92 @@
 # Usage:
 # ./reformat_gsav2-chip.sh <prefix_bfiles> <output_dir/>
 
-set -euo pipefail
+echo "[INFO] Start: $(date)"
+
+check_dx_file() {
+    # Checks the existence of a file
+    # within DNAnexus.
+    FILEPATH="$1"
+    if ! dx ls "$FILEPATH" &> /dev/null; then
+        echo "[ERROR] Missing file in DNAnexus: $FILEPATH $(date)"
+        exit 1
+    fi
+}
 
 # --------------------------------
 # 0. Set-up
 # --------------------------------
-if [[ $# -ne 2 ]]; then
-    echo "Usage: $0 <gsav2-chip.vcf.gz> <outdir/>"
+set -euo pipefail
+
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <gsav2-chip.vcf.gz>"
     exit 1
 fi
 
+# Inputs
 bed_prefix="$1"
-output_dir="$2"
-
-echo "[START] $(date) 'Reformating GSAv2-CHIP files'"
 
 # --------------------------------
-# 1. Convert to BED to BGEN with PLINK2
+# 01. Check inputs and outputs
+# MODIFY THESE PATHS TO MATCH YOUR PROJECT
 # --------------------------------
-echo "[INFO] $(date) | Running PLINK2 conversion"
-for i in {1..22}; do
-    bed_name=$(basename $bed_prefix)
-    output_name="${output_dir}/${bed_name}.chr${i}"
-    plink2 --bfile "$bed_prefix" \
-        --chr $i \
-        --export bgen-1.2 \
-        --rm-dup 'exclude-all' \
-        --snps-only \
-        --set-all-var-ids 'chr@:#:$r:$a' \
-        --out "${output_name}"
-done
-echo "[INFO] $(date) | Done!"
+
+INPUT_ROOT="/Data"
+GENO_DIR="${INPUT_ROOT}/GSAv2-Chip/data/pVCF/revised_qc"
+BED_FILE="${GENO_DIR}/${bed_prefix}.bed"
+BIM_FILE="${GENO_DIR}/${bed_prefix}.bim"
+FAM_FILE="${GENO_DIR}/${bed_prefix}.fam"
+
+OUTPUT_ROOT="/Users/Roberto/00_data/01_genotypes"
+OUTPUT_DIR="${OUTPUT_ROOT}/01_gsav2-chip"
+
+# Check existence in DNAnexus
+echo "[INFO] $(date) | Checking input files..."
+
+check_dx_file "$BED_FILE"
+check_dx_file "$BIM_FILE"
+check_dx_file "$FAM_FILE"
+
+# Create the output directory on DNAnexus
+if ! dx ls "$OUTPUT_DIR" &> /dev/null; then
+    echo "[INFO] $(date) | Creating output directory: $OUTPUT_DIR"
+    dx mkdir -p "$OUTPUT_DIR"
+fi
+
+# --------------------------------
+# 02. Launch Swiss Army Knife
+# --------------------------------
+
+# Job name
+JOB_NAME="BED to BGEN - Homogenize variant ID's and minimal QC"
+
+# DNAnexus Launch Command: Swiss Army Knife
+dx run app-swiss-army-knife \
+    -imount_inputs=true \
+    -iin="${BED_FILE}" \
+    -iin="${BIM_FILE}" \
+    -iin="${FAM_FILE}" \
+    --instance-type mem1_ssd2_v2_x8 \
+    --priority high \
+    --name "$JOB_NAME" \
+    --folder "$OUTPUT_DIR" \
+    -icmd="
+    # Get the base name without extension
+    base_name=\$(basename ${BED_FILE} .bed)
+    
+    for i in {1..22}; do
+        plink2 --bfile \$base_name \
+            --chr \$i \
+            --export bgen-1.2 \
+            --rm-dup 'exclude-all' \
+            --snps-only \
+            --geno 0.05 --maf 0.1 \
+            --set-all-var-ids 'chr@:#:\$r:\$a' \
+            --out \${base_name}-maf01.chr_\$i
+    done
+    " \
+    --brief \
+    --yes
+
+# Retrieving the job name
+echo "[DONE] $(date) | Job submitted: $JOB_NAME"
