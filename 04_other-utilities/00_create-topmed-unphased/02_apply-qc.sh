44e1ced3c5c532a29d9128f5e6fe7ad683d9b3e1
WSL - Roberto Olvera Hernandez <rolvhdez@ccg.unam.mx>
2026-03-05 14:59:59 -0600

    [changed] Reformat structure to work with 'dx find data'

--- 04_other-utilities/00_create-topmed-unphased/02_apply-qc.sh
+++ 04_other-utilities/00_create-topmed-unphased/02_apply-qc.sh
@@ -9,12 +9,12 @@ echo "[INFO] Start: $(date)"
 dxCheckFile() {
     # Checks the existence of a file
     # within DNAnexus.
-    FILEPATH="$1"
-    if ! dx ls "$FILEPATH" &> /dev/null; then
-        echo "[ERROR] Missing file in DNAnexus: $FILEPATH $(date)"
+    local FILE_ID="$1"
+    if ! dx ls "$FILE_ID" &> /dev/null; then
+        echo "[ERROR] Missing file in DNAnexus: $FILE_ID $(date)"
         exit 1
     fi
-    echo "[INFO] $(date) | Ready: $(basename $FILEPATH) "
+    echo "[INFO] Using: $FILE_ID"
 }
 
 # --------------------------------
@@ -28,23 +28,29 @@ if ! [[ "$CHR" =~ ^[0-9]+$ ]] || [[ "$CHR" -lt 1 ]] || [[ "$CHR" -gt 22 ]]; then
 fi
 CHR_PADDED=$(printf "%02d" "$CHR")
 
-GENO_DIR="/Users/Roberto/00_data/01_genotypes/02_topmed-imputed/00_unphased"
-BGEN="${GENO_DIR}/MCPS_Freeze_150.GT_hg38.pVCF.rgcpid.QC2.TOPMED_dosages.high-quality.bgen"
-SAMPLE="${GENO_DIR}/MCPS_Freeze_150.GT_hg38.pVCF.rgcpid.QC2.TOPMED_dosages.high-quality.sample"
-inputs=("$BGEN" "$SAMPLE")
-
-OUT_DIR="/Users/Roberto/00_data/01_genotypes/02_topmed-imputed/00_unphased/"
-OUT_NAME="MCPS_Freeze_150.GT_hg38.pVCF.rgcpid.QC2.TOPMED_dosages.high-quality.applied-qc.chr_${CHR}"
+# Find the ID's of the required files
+BGEN=$(
+dx find data \
+  --tag 'topmed-imputed' --tag 'unphased' --tag 'high-quality' \
+  --name "*chr_${CHR}.bgen" --brief
+) 
+SAMPLE=$(
+dx find data \
+  --tag 'topmed-imputed' --tag 'unphased' --tag 'high-quality' \
+  --name "*chr_${CHR}.sample" --brief
+) 
+UNRELATED=$(
+dx find data \
+  --tag 'unrelated-individuals' --tag 'sample-list' \
+  --name "*.txt" --brief
+)
 
 # Check ---
 echo "[INFO] $(date) | Checking input files..."
+
+inputs=("$BGEN" "$SAMPLE" "$UNRELATED")
 for i in "${inputs[@]}"; do dxCheckFile "$i"; done
 echo "[INFO] $(date) | All required inputs exist."
-
-if ! dx ls "$OUT_DIR" &> /dev/null; then
-    echo "[INFO] $(date) | Directory does not exist: $OUT_DIR"
-    exit 1
-fi
 
 # --------------------------------
 # 03. Launch Swiss Army Knife
@@ -52,24 +58,41 @@ fi
 
 JOB_NAME="Apply QC TOPMed-imputed (Unphased) (chr_${CHR_PADDED})"
 
+BGEN_NAME=$(dx describe "$BGEN" --json | jq -r .name)
+SAMPLE_NAME=$(dx describe "$SAMPLE" --json | jq -r .name)
+UNRELATED_NAME=$(dx describe "$UNRELATED" --json | jq -r .name)
+
+# Output directory and file name
+OUT_DIR="/Users/Roberto/00_data/01_genotypes/02_topmed-imputed/00_unphased/"
+OUT_NAME="MCPS_Freeze_150.GT_hg38.pVCF.rgcpid.QC2.TOPMED_dosages.high-quality.maf-01.unrelated.chr_${CHR}"
+if ! dx ls "$OUT_DIR" &> /dev/null; then
+    echo "[INFO] $(date) | Directory does not exist: $OUT_DIR"
+    exit 1
+fi
+
+# Execute the launcher
 dx run app-swiss-army-knife \
   -imount_inputs=true \
   -iin="$BGEN" \
   -iin="$SAMPLE" \
-  -iin="$CHR" \
+  -iin="$UNRELATED" \
   --instance-type mem1_ssd1_v2_x8 \
   --priority high \
   --name "$JOB_NAME" \
   --folder "$OUT_DIR" \
   -icmd="
-  plink2 --bgen '$(basename $BGEN)' 'ref-first' \
-    --sample '$(basename $SAMPLE)' \
-    --chr '${CHR}'
+  #
+  plink2 --bgen '${BGEN_NAME}' 'ref-first' \
+    --sample '${SAMPLE_NAME}' \
+    --keep '${UNRELATED_NAME}' \
     --snps-only --rm-dup 'exclude-all' \
     --geno 0.05 --maf 0.01 \
+    --make-pgen \
+    --out 'tmpfile' && \
+  plink2 --pfile 'tmpfile' \
     --export bgen-1.2 \
-    --memory 12000 --threads 8 \
     --out '${OUT_NAME}'
   " \
   --cost-limit 3 \
+  --yes \
   --brief
