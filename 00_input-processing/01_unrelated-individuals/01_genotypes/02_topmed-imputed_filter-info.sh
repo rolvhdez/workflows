b679d194f40c889b2d7fbe1987ffadc24b57681c
CW - Roberto Olvera Hernandez <rolvhdez@ccg.unam.mx>
2026-03-06 00:22:41 +0000

    [changed] Using the dx find method

--- 00_input-processing/01_unrelated-individuals/01_genotypes/02_topmed-imputed_filter-info.sh
+++ 00_input-processing/01_unrelated-individuals/01_genotypes/02_topmed-imputed_filter-info.sh
@@ -9,12 +9,12 @@ echo "[INFO] Start: $(date)"
 dxCheckFile() {
     # Checks the existence of a file
     # within DNAnexus.
-    FILEPATH="$1"
-    if ! dx ls "$FILEPATH" &> /dev/null; then
-        echo "[ERROR] Missing file in DNAnexus: $FILEPATH $(date)"
+    local FILE_ID="$1"
+    if ! dx ls "$FILE_ID" &> /dev/null; then
+        echo "[ERROR] Missing file in DNAnexus: $FILE_ID $(date)"
         exit 1
     fi
-    echo "[INFO] $(date) | Ready: $(basename $FILEPATH) "
+    echo "[INFO] Using: $FILE_ID"
 }
 
 # --------------------------------
@@ -28,18 +28,12 @@ if ! [[ "$CHR" =~ ^[0-9]+$ ]] || [[ "$CHR" -lt 1 ]] || [[ "$CHR" -gt 22 ]]; then
 fi
 CHR_PADDED=$(printf "%02d" "$CHR")
 
-# Fixed files
-BGEN="/Data/TOPMED-imputed/MCPS_Freeze_150.GT_hg38.pVCF.rgcpid.QC2.TOPMED_dosages.bgen" # ID is provided below
-SAMPLE="/Data/TOPMED-imputed/MCPS_Freeze_150.GT_hg38.pVCF.rgcpid.QC2.TOPMED_dosages.COLLAB.sample" # ID is provided below
-SNPLIST="/Users/Roberto/00_data/01_genotypes/02_topmed-imputed/rgcpid.QC2.TOPMED_dosages.high-quality.snpstats.txt.gz" # INFO > 0.99
-
-inputs=("$BGEN" "$SAMPLE" "$SNPLIST")
-
-# Job name
-OUT_DIR="/Users/Roberto/00_data/01_genotypes/02_topmed-imputed/00_unphased/"
-OUT_NAME="MCPS_Freeze_150.GT_hg38.pVCF.rgcpid.QC2.TOPMED_dosages.high-quality.chr_${CHR}"
+BGEN="file-G36G1VQ0b67BZ5PK4ZFQ4qF9" # ID is provided below
+SAMPLE="file-G46kY3805vBvbg8q6J5357B4" # ID is provided below
+SNPLIST=$(dx find data --tag 'topmed-imputed' --tag 'snplist' --name '*.txt.gz') # INFO > 0.99
 
 # Check inputs
+inputs=("$BGEN" "$SAMPLE" "$SNPLIST")
 echo "[INFO] $(date) | Checking input files..."
 for i in "${inputs[@]}"; do dxCheckFile "$i"; done
 echo "[INFO] $(date) | All required inputs exist."
@@ -55,6 +49,12 @@ fi
 # --------------------------------
 
 JOB_NAME="Segment TOPMed-imputed (Unphased) INFO > 0.99 (chr_${CHR_PADDED})"
+BGEN_NAME="$(dx describe $BGEN --json | jq -r .name)"
+SAMPLE_NAME="$(dx describe $SAMPLE --json | jq -r .name)"
+SNPLIST_NAME="$(dx describe $SNPLIST --json | jq -r .name)"
+
+OUT_DIR="/Users/Roberto/00_data/01_genotypes/02_topmed-imputed/00_unphased/"
+OUT_NAME="MCPS_Freeze_150.GT_hg38.pVCF.rgcpid.QC2.TOPMED_dosages.high-quality.chr_${CHR}"
 
 # DNAnexus Launch Command: Swiss Army Knife
 # Instance: mem1_ssd1_v2_x8 (CPU : 8 | RAM : 16.0 GB | VOL : 200 GB)
@@ -68,16 +68,17 @@ dx run app-swiss-army-knife \
   --name "$JOB_NAME" \
   --folder "$OUT_DIR" \
   -icmd="
-  plink2 --bgen '$(basename $BGEN)' 'ref-first' \
-    --sample '$(basename $SAMPLE)' \
-    --extract '$(basename $SNPLIST)' \
+  plink2 --bgen '${BGEN_NAME}' 'ref-first' \
+    --sample '${SAMPLE_LIST}' \
+    --extract '${SNPLIST_NAME}' \
     --chr '${CHR}' \
     --export bgen-1.2 \
     --memory 12000 --threads 8 \
     --out '${OUT_NAME}'
   " \
   --cost-limit 3 \
-  --brief
+  --brief \
+  --yes
 
 # Retrieving the job name
 echo "[DONE] $(date) | Job submitted: $JOB_NAME"
